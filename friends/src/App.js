5bcb8b8e170883e00c96f0ee882af49bfd8c05aa
Maxwell Beard <maxwelljamesbeard@gmail.com>
2019-12-17 18:07:30 +0000

    added delete functionality aswell as add friend

--- friends/src/App.js
+++ friends/src/App.js
@@ -5,12 +5,14 @@ import { PrivateRoute } from "./components/PrivateRoute";
 
 import Login from "./components/Login";
 import FriendsList from "./components/FriendsList";
+import Friend from "./components/Friend";
 
 function App() {
   return (
     <div className="App">
       <Route path="/login" component={Login} />
       <PrivateRoute exact path="/friends" component={FriendsList} />
+      <PrivateRoute path="/friends/:id" component={Friend} />
     </div>
   );
 }
--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -1,8 +1,12 @@
 import React, { useState, useEffect } from "react";
+import { NavLink } from "react-router-dom";
 import { axiosWithAuth } from "../axios/axiosWithAuth";
+import AddFriends from "./AddFriends";
+// import "./friendslist.css";
 
 export default function FriendsList() {
   const [friends, setFriends] = useState([]);
+  const [editMode, setEditMode] = useState(false);
   const [friend, setFriend] = useState({
     name: "",
     age: "",
@@ -21,11 +25,77 @@ export default function FriendsList() {
       });
   }, []);
 
+  const addFriend = () => {
+    axiosWithAuth()
+      .post("http://localhost:5000/api/friends", friend)
+      .then(res => {
+        console.log(res.data);
+        console.log("friend added!");
+        setFriends(res.data);
+        setFriend({ name: "", age: "", email: "" });
+      })
+      .catch(err => {
+        console.log(err);
+        alert("An error occurred!");
+      });
+  };
+
+  const populateField = friend => {
+    setFriend(friend);
+    setEditMode(true);
+  };
+
+  //   const editFriend = () => {
+  //     axiosWithAuth()
+  //       .put(`http://localhost:5000/api/friends/${friend.id}`, friend)
+  //       .then(res => {
+  //         console.log(res.data);
+  //         console.log("edited friend!");
+  //         setFriends(res.data);
+  //       })
+  //       .catch(err => {
+  //         console.log(err);
+  //         alert("An error occurred!");
+  //       });
+  //   };
+  const deleteFriend = id => {
+    axiosWithAuth()
+      .delete(`http://localhost:5000/api/friends/${id}`)
+      .then(res => {
+        console.log(res.data);
+        console.log("deleted friend!");
+        setFriends(res.data);
+      })
+      .catch(err => {
+        console.log(err);
+        alert("An error occurred!");
+      });
+  };
+
+  const handleInput = e =>
+    setFriend({ ...friend, ...{ [e.target.name]: e.target.value } });
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    // console.log("friends = ",friends);
+    // console.log("friend ",friend)
+
+    const found = friends.find(el => el.id === friend.id);
+    if (!found) addFriend(friend.id);
+  };
+
   return (
     <div>
-      <table>
+      <AddFriends
+        friend={friend}
+        handleInput={handleInput}
+        handleSubmit={handleSubmit}
+        editMode={Boolean(friends.find(el => el.id === friend.id))}
+      />
+      &nbsp;&nbsp;
+      <table className="friendslist-table">
         <thead>
-          <tr>
+          <tr className="friendslist-table-header">
             <td>ID</td>
             <td>Name</td>
             <td>AGE</td>
@@ -36,11 +106,18 @@ export default function FriendsList() {
         <tbody>
           {friends.map(friend => {
             return (
-              <tr key={friend.id}>
+              <tr key={friend.id} className="friendslist-table-item">
                 <td>{friend.id}</td>
                 <td>{friend.name}</td>
                 <td>{friend.age}</td>
                 <td>{friend.email}</td>
+                <td>
+                  <NavLink to={`friends/${friend.id}`}>view</NavLink>
+                  &nbsp;&nbsp;
+                  <button onClick={() => deleteFriend(friend.id)}>
+                    Delete
+                  </button>
+                </td>
               </tr>
             );
           })}
